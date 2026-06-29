efc68e8f61388c7064b10cb5ebb3bd5c755cf33e
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-06-29 19:13:02 +0200

    Updates

--- script/listItem.js
+++ script/listItem.js
@@ -1,6 +1,6 @@
 export default function listItem({ name, type, img }) {
   return `
-    <li class="friends__item" aria-label="Open details">
+    <li class="friends__item" aria-label="Open details" id=${name}>
       <img src="${img}" alt="${type} ${name}">
       <h3 class="friends__item-title">${name}</h3>
       <span class="btn btn_secondary">Learn more</span>
--- script/petsList.js
+++ script/petsList.js
@@ -1,10 +1,23 @@
 import listItem from './listItem.js';
 import itemsPerPage from './itemsPerPage.js';
 import { pagination } from './currentPageState.js';
+import petPopup from './petPopup.js';
 
 export default async function petsList(data) {
   const petsListEl = document.getElementById('pets-list');
   petsListEl.innerHTML = '';
+
+  petsListEl.addEventListener('click', function (e) {
+    if (e.target.closest('.friends__item')) {
+      const currentPet = data.find(
+        (el) => el.name === e.target.closest('.friends__item').id,
+      );
+      console.log(currentPet);
+      const popup = petPopup(currentPet);
+      document.body.append(popup);
+      document.body.classList.add('non-scroll');
+    }
+  });
 
   const currentPage = pagination.currentPage;
   const itemsPerCurrentPage = itemsPerPage();
