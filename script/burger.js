8e470ab8693e521ff4dab03578ee6d7668a3cc27
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-06-30 18:51:46 +0200

    fix: correct header menu on tablet and mobile

--- script/burger.js
+++ script/burger.js
@@ -11,10 +11,16 @@ export default function burger() {
   }
 
   nav.addEventListener('click', function (e) {
-    if (e.target.classList.contains('header__menu-link')) {
+    if (
+      e.target.classList.contains('header__menu-link') ||
+      !e.target.closest('.header__menu')
+    ) {
       burger.classList.remove('burger_open');
       nav.classList.remove('header__nav_open');
       document.body.classList.remove('non-scroll');
+    }
+
+    if (!e.target.closest('.header__menu')) {
     }
   });
 }
